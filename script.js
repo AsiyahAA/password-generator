e1c5e800befa62e9553cd38ed2d106fd5f136b59
Asiya <asiyaahmed33@yahoo.com>
2022-06-27 16:01:29 -0700

    final version

--- script.js
+++ script.js
@@ -14,12 +14,13 @@ var availableCharacters  = []
 generateBtn.addEventListener("click", startPassword);
 
 function startPassword() {
+  availableCharacters = []
   alert("Password must be at least 8 characters long but no more than 128 characters. Select OK to continue", "OK");
   passwordLength();
 }
 
 function passwordLength() {
-  length = prompt("How many characters would you like in your password?");
+  length = parseInt(prompt("How many characters would you like in your password?"));
 
   if(length >=8 &&  length <=128) {
     alert("Great! Password is between 8 and 128 characters. Let's continue!");
@@ -39,7 +40,7 @@ function lowerCaseLetters() {
 
 
   if(lowerCase) {
-    availableCharacters.concat(["abcdefghijklmnopqrstuvwxyz"])
+  availableCharacters = availableCharacters.concat(["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"])
   } 
   upperCaseLetters()
 }
@@ -49,7 +50,7 @@ function upperCaseLetters() {
   var upperCase = confirm("Would you like to use uppercase letters in your password? Select OK for 'Yes' or Cancel for 'No'");
 
   if(upperCase) {
-    availableCharacters.concat(["ABCDEFGHIJKLMOPQRSTUVWXYZ"])
+  availableCharacters = availableCharacters.concat(["A","B","C","D","E","F","G","H","I","J","K","L","M","O","P","Q","R","S","T","U","V","W","X","Y","Z"])
   }
   passwordNumbers()
   }
@@ -59,7 +60,7 @@ function passwordNumbers() {
   var numbers = confirm("Would you like numbers in your password? Select OK for 'Yes' or Cancel for 'No'");
 
   if(numbers) {
-  availableCharacters.concat([0,1,2,3,4,5,6,7,8,9])
+  availableCharacters = availableCharacters.concat([0,1,2,3,4,5,6,7,8,9])
   }
   specialCharacters()
 }
@@ -68,20 +69,20 @@ function specialCharacters() {
   var special = confirm("Would you like special characters in your password? Select OK for 'Yes' or Cancel for 'No'");
 
   if(special) {
-  availableCharacters.concat(["!@#$%^&*+(),./?><{}[]`~-_=|:;'"])
+  availableCharacters = availableCharacters.concat(["!","@","#","$","%","^","&","*","+","(",")",",",".","/","?",">","<","{","}","[","]","`","~","-","_","=","|",":",";","'"])
   }
-  // writePassword()
+    writePassword() }
+
+function writePassword() {
+  console.log(availableCharacters)
+  var password = ""
+  for (i = 0; i<length; i++) {
+  var randomIndex = Math.floor(Math.random() * availableCharacters.length)
+  var character = availableCharacters[randomIndex]
+  console.log(character)
+  password += character
+  }
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
 }
 
-
-// var generatePassword
-
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-// }
-// for (i = 0; i<length; i++) {
-//   password += avaMath.floor(Math.random)
-// }
