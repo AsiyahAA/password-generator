b478edb1597412021609237a5820d6e1ccb11369
Asiya <asiyaahmed33@yahoo.com>
2022-06-27 12:57:33 -0700

    corrected if/else statements

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+var length; 
+var availableCharacters  = []
 // Write password to the #password input
 // function writePassword() {
 //   var password = generatePassword();
@@ -14,85 +15,64 @@ generateBtn.addEventListener("click", startPassword);
 
 function startPassword() {
   alert("Password must be at least 8 characters long but no more than 128 characters. Select OK to continue", "OK");
+  passwordLength();
 }
-
-var passwordLength
-generateBtn.addEventListener("click", passwordLength);
 
 function passwordLength() {
-  prompt("How many characters would you like in your password?");
-}
+  length = prompt("How many characters would you like in your password?");
 
-if(passwordLength >8 && passwordLength ==8 && passwordLength <128 && passwordLength ==128) {
-  alert("Great! Password is between 8 and 128 characters. Let's continue!");
-}
-
-else if(passwordLength <8 && passwordLength >128) {
-  alert("Your password length does not fall between 8 and 128 characters. Please try again.");
+  if(length >=8 &&  length <=128) {
+    alert("Great! Password is between 8 and 128 characters. Let's continue!");
+    lowerCaseLetters();
+  } else {
+    alert("Your password length does not fall between 8 and 128 characters. Please try again.");
+    startPassword();
+  }
 }
 
 // else
 //   alert("No password length inputed. Please input a desired character length for your password");
 
-var lowerCaseLetters
-generateBtn.addEventListener("click", lowerCaseLetters);
-
 function lowerCaseLetters() {
-  confirm("Would you like to use lowercase letters in your password? Select OK for 'Yes' or Cancel for 'No'");
-}
 
-if(lowerCaseLetters == 'OK') {
-  alert("You have selected to include lowercase letters in your password")
-}
+  var lowerCase = confirm("Would you like to use lowercase letters in your password? Select OK for 'Yes' or Cancel for 'No'");
 
-else if(lowerCaseLetters == 'Cancel') {
-  alert("You have selected not to include lowercase letters in your password")
-}
 
-var upperCaseLetters
-generateBtn.addEventListener("click", upperCaseLetters);
+  if(lowerCase) {
+    availableCharacters.concat(["abcdefghijklmnopqrstuvwxyz"])
+  } 
+  upperCaseLetters()
+}
 
 function upperCaseLetters() {
-  confirm("Would you like to use uppercase letters in your password? Select OK for 'Yes' or Cancel for 'No'");
-}
+  
+  var upperCase = confirm("Would you like to use uppercase letters in your password? Select OK for 'Yes' or Cancel for 'No'");
 
-if(upperCaseLetters == 'OK') {
-  alert("You have selected to include uppercase letters in your password")
-}
-
-else if(upperCaseLetters == 'Cancel') {
-  alert("You have selected not to include uppercase letters in your password")
-}
-
-var passwordNumbers
-generateBtn.addEventListener("click", passwordNumbers);
+  if(upperCase) {
+    availableCharacters.concat(["ABCDEFGHIJKLMOPQRSTUVWXYZ"])
+  }
+  passwordNumbers()
+  }
 
 function passwordNumbers() {
-  confirm("Would you like numbers in your password? Select OK for 'Yes' or Cancel for 'No'");
-}
+  
+  var numbers = confirm("Would you like numbers in your password? Select OK for 'Yes' or Cancel for 'No'");
 
-if(passwordNumbers == 'OK') {
-  alert("You have selected to include numbers in your password")
+  if(numbers) {
+  availableCharacters.concat([0,1,2,3,4,5,6,7,8,9])
+  }
+  specialCharacters()
 }
-
-else if(passwordNumbers == 'Cancel') {
-  alert("You have selected not to include numbers in your password")
-}
-
-var specialCharacters
-generateBtn.addEventListener("click", specialCharacters);
 
 function specialCharacters() {
-  confirm("Would you like special characters in your password? Select OK for 'Yes' or Cancel for 'No'");
+  var special = confirm("Would you like special characters in your password? Select OK for 'Yes' or Cancel for 'No'");
+
+  if(special) {
+  availableCharacters.concat(["!@#$%^&*+(),./?><{}[]`~-_=|:;'"])
+  }
+  // writePassword()
 }
 
-if(specialCharacters == 'OK') {
-  alert("You have selected to include special characters in your password")
-}
-
-else if(specialCharacters == 'Cancel') {
-  alert("You have selected not to include special characters in your password")
-}
 
 // var generatePassword
 
@@ -101,4 +81,7 @@ else if(specialCharacters == 'Cancel') {
 //   var passwordText = document.querySelector("#password");
 
 //   passwordText.value = password;
+// }
+// for (i = 0; i<length; i++) {
+//   password += avaMath.floor(Math.random)
 // }
